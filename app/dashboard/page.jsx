67fa0bcec56bb3de059b7298f4d5d2928bbda8a2
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-01-04 00:50:24 +0530

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,7 +7,7 @@ import InterviewList from './_components/InterviewList'
 function Dashboard() {
   return (
     <div className='p-10'>
-      <h2 className='font-bold text-3xl'></h2>
+      <h2 className='font-bold text-3xl'>Dashboard</h2>
       <h2>Create and Start Your Mock Interview</h2>
       <div className='grid grid-col-1 md:grid-cols-3 my-5'>
       <AddNewInterview />
